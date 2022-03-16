8c2b7a3498468501a194c1c8056339e4c639c6fb
RITWEEK RAJ <67289913+ritweekraj2802@users.noreply.github.com>
2022-03-16 22:42:15 +0530

    Time: 3 ms (36.73%), Space: 41.2 MB (46.52%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int f) {
+        
+            long x=f;
+            long low = 1, high = x, ans = 0;
+
+            while(low <= high)
+            {
+                long mid = (low + high) / 2;
+
+                long mSq = mid * mid;
+
+                if(mSq == x)
+                    return (int)mid;
+                else if(mSq > x)
+                    high = mid - 1;
+                else
+                {
+                    low = mid + 1;
+                    ans = mid;
+                }
+            }
+
+            return (int)ans;
+    }
+}
