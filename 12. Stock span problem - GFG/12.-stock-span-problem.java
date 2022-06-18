a92af7e583b08428138775a6a3909da99fde9be6
RITWEEK RAJ <67289913+ritweekraj2802@users.noreply.github.com>
2022-06-18 22:10:39 +0530

    Added solution - LeetHub

---
+++ 12. Stock span problem - GFG/12.-stock-span-problem.java
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int a[] = new int[n];
+            
+            int i = 0;
+            for(i = 0; i < n; i++)
+              a[i] = sc.nextInt();
+              
+            int[] s = new Solution().calculateSpan(a, n);
+            
+            for(i = 0; i < n; i++)
+            {
+                System.out.print(s[i] + " ");
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    
+    
+}// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to calculate the span of stockâ€™s price for all n days.
+    public static int[] calculateSpan(int arr[], int n)
+    {
+        // Your code here
+        Stack<Integer> s=new Stack<>() ;
+        int[] a=new int[n];
+        s.add(0);
+        
+        int x=0;
+        a[x++]=1;
+        for(int i=1;i<n;i++){
+            while(s.isEmpty()==false && arr[s.peek()]<=arr[i]){
+                s.pop();
+            }
+            int span=s.isEmpty() ? i+1 : i-s.peek();
+            
+            a[x++]=span;
+            s.push(i);
+        } 
+        return a;
+    }
+    
+}
